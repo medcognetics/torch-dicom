2d5000ec6a597934be2adf9da3ea868b559b4ef6
Scott Chase Waggener <tidalpaladin@protonmail.com>
2023-12-01 11:33:28 -0600

    Add sampler for batched complements

--- tests/test_datasets/test_sampler.py
+++ tests/test_datasets/test_sampler.py
@@ -1,11 +1,13 @@
 import math
 from pathlib import Path
+from typing import List
 
+import numpy as np
 import pandas as pd
 import pytest
 import torch
 
-from torch_dicom.datasets.sampler import WeightedCSVSampler
+from torch_dicom.datasets.sampler import BatchComplementSampler, WeightedCSVSampler
 
 
 class TestWeightedCSVSampler:
@@ -49,3 +51,69 @@ class TestWeightedCSVSampler:
         sampler = WeightedCSVSampler(metadata, paths, "value", weight, num_samples=1000)
         result = [idx % 2 for idx in sampler]
         assert math.isclose(sum(result) / len(result), 0.75, abs_tol=0.05)
+
+
+class TestBatchComplementSampler:
+    @pytest.fixture
+    def paths(self):
+        return [Path(f"sop-{i}").with_suffix(".dcm") for i in range(10)]
+
+    @pytest.fixture
+    def batch_size(self):
+        return 4
+
+    @pytest.fixture
+    def complement_size(self):
+        return 2
+
+    @pytest.fixture
+    def metadata(self, tmp_path, paths):
+        path = tmp_path / "metadata.csv"
+        df = pd.DataFrame(
+            {
+                "SOPInstanceUID": [p.stem for p in paths],
+                "StudyInstanceUID": [f"study-{i // 4}" for i in range(len(paths))],
+            }
+        )
+        df.to_csv(path, index=False)
+        return path
+
+    @pytest.fixture
+    def sampler(self, metadata, paths, batch_size, complement_size):
+        class BatchStudySampler(BatchComplementSampler):
+            @property
+            def complement_size(self):
+                return complement_size
+
+            def find_complement(self, idx: int) -> List[int]:
+                # Get the SOP and study ID of the example
+                sop = self.metadata.iloc[idx].name
+                study_id = self.metadata.iloc[idx]["StudyInstanceUID"]
+
+                # Get random SOPUIDs of other examples in the same study, excluding the example itself
+                same_study_sops = self.metadata[self.metadata["StudyInstanceUID"] == study_id].index.tolist()
+                if not same_study_sops:
+                    return [idx] * self.complement_size
+                same_study_sops.remove(sop)
+                needs_replacement = len(same_study_sops) < self.complement_size
+                complement_sops = np.random.choice(
+                    same_study_sops, self.complement_size, replace=needs_replacement
+                ).tolist()  # type: ignore
+
+                # Convert SOPUIDs to indexes
+                complement_indexes = [self.metadata.index.get_loc(sop) for sop in complement_sops]
+
+                return complement_indexes
+
+        return BatchStudySampler(range(len(paths)), batch_size, metadata, paths)
+
+    def test_len(self, sampler, paths, complement_size, batch_size):
+        assert len(sampler) == len(paths) * complement_size // batch_size
+
+    def test_sampler(self, sampler, batch_size):
+        output = list(sampler)
+        assert isinstance(output, list)
+        assert len(output) == len(sampler)
+        for i in output:
+            assert isinstance(i, list)
+            assert len(i) == batch_size
